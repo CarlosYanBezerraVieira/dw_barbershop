548129b7d4095d5fa57cf6e10106d7cd0574ec7f
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-02-28 20:24:42 -0300

    feat:create constants of colors, font and path of images

---
+++ dw-barbershop/lib/src/core/ui/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+sealed class ColorsConstants {
+  static const brow = Color(0xFFB07B01);
+  static const grey = Color(0xFF999999);
+  static const greyLight = Color(0xFFE6E2E9);
+  static const red = Color(0xFFEB1212);
+}
+
+sealed class ImagesConstants {
+  static const logo = 'assets/images/imgLogo.png';
+  static const background = 'assets/images/background_image_chair.jpg';
+}
+
+sealed class FontsConstants {
+  static const fontFamily = 'Poppins';
+}
