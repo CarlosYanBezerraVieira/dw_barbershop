f07d0c52081b7c4ea4d4ac5b0481482d8b24cb97
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-02-28 20:26:36 -0300

    feat: create login page and your route

--- dw-barbershop/lib/src/barbershop_app.dart
+++ dw-barbershop/lib/src/barbershop_app.dart
@@ -1,5 +1,6 @@
 import 'package:asyncstate/asyncstate.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/barbeshop_loader.dart';
+import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
 import 'package:dw_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,10 @@ class BarbershopApp extends StatelessWidget {
             navigatorObservers: [asyncNavigatorObserver],
             title: 'DW Barbershop',
             debugShowCheckedModeBanner: false,
-            routes: {'/': (_) => const SplashPage()},
+            routes: {
+              '/': (_) => const SplashPage(),
+              '/auth/login': (_) => const LoginPage(),
+            },
           );
         });
   }
